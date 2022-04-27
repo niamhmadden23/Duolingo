2262717c9e53f357a659b95af79921bbaff13a22
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-27 16:39:07 +0100

    Question with styling added

---
+++ App.styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "stretch",
+    padding: 10,
+  },
+});
+
+export default style;
