27f51d0b86ddb05523345cf4203ea433a8874e87
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-27 16:58:51 +0100

    Added styling for question page

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import style from "./App.styles";
 
 const App = () => {
   return (
     <View style={style.root}>
       <Text style={style.title}>Which of these is the "glass"?</Text>
+      <View style={style.optionsContainer}>
+        <View style={style.optionContainer}>
+          <Image
+            source={{
+              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png",
+            }}
+            style={style.optionImage}
+          />
+          <Text style={style.optionText}>Glass</Text>
+        </View>
+        <View style={style.optionContainer}>
+          <Image
+            source={{
+              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png",
+            }}
+            style={style.optionImage}
+          />
+          <Text style={style.optionText}>Glass</Text>
+        </View>
+        <View style={style.optionContainer}>
+          <Image
+            source={{
+              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png",
+            }}
+            style={style.optionImage}
+          />
+          <Text style={style.optionText}>Glass</Text>
+        </View>
+        <View style={style.optionContainer}>
+          <Image
+            source={{
+              uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png",
+            }}
+            style={style.optionImage}
+          />
+          <Text style={style.optionText}>Glass</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- App.styles.js
+++ App.styles.js
@@ -12,6 +12,26 @@ const style = StyleSheet.create({
     alignSelf: "stretch",
     padding: 10,
   },
+  optionContainer: {
+    borderWidth: 2,
+    borderBottomWidth: 4,
+    borderColor: "lightgrey",
+    borderRadius: 10,
+    width: "50%",
+    height: "50%",
+  },
+  optionsContainer: {
+    backgroundColor: "red",
+    width: "100%",
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  optionImage: {
+    width: 100,
+    height: 100,
+  },
+  optionText: {},
 });
 
 export default style;
