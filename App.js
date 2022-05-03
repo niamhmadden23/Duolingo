eee4b93a910ee38fe69a6563491530183842f5a4
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-05-03 10:50:29 +0100

    mapping images and text

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Text, View, Image } from "react-native";
 import style from "./App.styles";
 import ImageOption from "./src/components/ImageOption";
+import question from "./assets/data/oneQuestionWithOption";
 
 const App = () => {
   return (
     <View style={style.root}>
-      <Text style={style.title}>Which of these is the "glass"?</Text>
+      <Text style={style.title}>{question.question}</Text>
       <View style={style.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        {question.options.map((option) => (
+          <ImageOption image={option.image} text={option.text} />
+        ))}
       </View>
     </View>
   );
--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
 import style from "./styles";
-const ImageOption = () => {
-  return (
-    <View style={style.optionContainer}>
-      <Image
-        source={{
-          uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png",
-        }}
-        resizeMode="contain"
-        style={style.optionImage}
-      />
-      <Text style={style.optionText}>Glass</Text>
-    </View>
-  );
-};
+const ImageOption = ({ image, text }) => (
+  <View style={style.optionContainer}>
+    <Image
+      source={{
+        uri: image,
+      }}
+      resizeMode="contain"
+      style={style.optionImage}
+    />
+    <Text style={style.optionText}>{text}</Text>
+  </View>
+);
 
 export default ImageOption;
